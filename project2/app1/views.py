5f91ef76b2f884b67eee63d2e79ccc093f850f47
Mahalaxmi <mahalaxmibejugam@gmail.com>
2020-08-19 22:35:47 +0530

    My Second project

--- project2/app1/views.py
+++ project2/app1/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 import operator
 
 # Create your views here.
-def home(request):
-	return render(request,'app1/home.html',{'name':'Maha'})
+def home(requests):
+	return render(requests,'app1/home.html',{'name':'Maha'})
 def aboutus(request):
 	return render(request,'app1/about.html',{'userid':'mahauserid'})
 def hobbies(request):
