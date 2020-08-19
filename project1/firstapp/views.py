87f6248de89485bbeae386f8baac13f75c961210
Mahalaxmi <mahalaxmibejugam@gmail.com>
2020-08-19 22:31:16 +0530

    My first project

--- project1/firstapp/views.py
+++ project1/firstapp/views.py
@@ -4,8 +4,8 @@ import operator
 
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1> This is my page</h1>')
-def aboutus(request):
-	return HttpResponse('<h1>I go to 3rd year B.E IT in vasavi</h1>')
+	return HttpResponse('<h1> This is my home page. I am Mahalaxmi. My roll number:1602-18-737-080</h1>')
+def aboutme(request):
+	return HttpResponse('<h1>I am Mahalaxmi 3rd year graduating from VCE in the  stream of IT bearing roll number 1602-18-737-080</h1>')
 def hobbies(request):
-	return HttpResponse('<h1> My hobbies are listening to music and playing badminton</h1>')
+	return HttpResponse('<h1> I like to code, listen music and learn new things.</h1>')
