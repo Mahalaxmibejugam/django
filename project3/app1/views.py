f414c0be429e6e1158ca81321b86bfcdf74412e1
Mahalaxmi <mahalaxmibejugam@gmail.com>
2020-08-24 00:07:18 +0530

    Third project

--- project3/app1/views.py
+++ project3/app1/views.py
@@ -8,5 +8,13 @@ def home(request):
 def count(request):
 	mytext=request.GET['fulltext']
 	wcount=len(mytext.split())
+	vList=list("aeiouAEIOU")
+	new_str=""
+	new_count=0
+	for i in mytext:
+		if i in vList:
+			new_count+=1
+		else:
+			new_str+=i
 	myList=[('rn1','Maha'), ('rn2','Ram'),('rn3','Shyam'),('rn4','Sam')]
-	return render(request, 'app1/count.html',{'mycount':wcount , 'yourlist' :myList})
+	return render(request, 'app1/count.html',{'count':wcount , 'myCount':new_count, 'mystr': new_str,'yourlist' :myList})
